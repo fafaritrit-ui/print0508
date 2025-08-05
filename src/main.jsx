650804e3e3529614f4c641a1e80733fb95922669
fafaritrit-ui <fafaritrit@gmail.com>
2025-08-05 15:28:41 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import "./firebaseConfig"; // Hanya perlu mengimpor, karena sudah di-inisialisasi di dalamnya
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -8,3 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
