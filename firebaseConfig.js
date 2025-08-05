d9a9d1052451ceba3ea7f05d1a1326e35bed365e
fafaritrit-ui <fafaritrit@gmail.com>
2025-08-05 15:28:05 +0700

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAowm2qfnTNKNy7jV3xRDn25vwvJAj8X3k",
+  authDomain: "testpost-da1ab.firebaseapp.com",
+  projectId: "testpost-da1ab",
+  storageBucket: "testpost-da1ab.firebasestorage.app",
+  messagingSenderId: "916388427815",
+  appId: "1:916388427815:web:90e0c8d27c7f6347f73602",
+  measurementId: "G-R4KDPY932D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
